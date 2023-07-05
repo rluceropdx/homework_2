30e56a51db440cb9c1a63759ffacedd1037f9edc
Casey Bailey <caseybailey@gmail.com>
2023-07-05 16:15:30 -0500

    WIP

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,55 @@
 use std::error::Error;
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
+use axum::Router;
+use axum::routing::{get, MethodRouter};
 use hyper::{Body, Method, Response};
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use tokio::net::TcpListener;
+use dotenvy::dotenv;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    init_logging();
+    
+    let addr = get_host_from_env();
+
+    let app = Router::new()
+        .route("/questions", get(|| async { "Hello world!!!"}));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
 
-
-pub struct Question {
-    pub id: QuestionId,
-    pub title: String,
-    pub content: String,
-    pub tag: Option<String>,
 }
 
-impl Question {
-    fn new(id: QuestionId, title: String, content: String, tag: Option<String>) -> Self {
-        Question {
-            id,
-            title,
-            content,
-            tag,
-        }
-    }
+fn get_host_from_env() -> SocketAddr {
+    let host = std::env::var("API_HOST").unwrap();
+    let api_host = IpAddr::from_str(&host).unwrap();
+    let api_port: u16 = std::env::var("API_PORT")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    SocketAddr::from((api_host, api_port))
 }
 
-pub struct QuestionId(pub String);
-
+fn init_logging() {
+    // https://github.com/tokio-rs/axum/blob/main/examples/tracing-aka-logging
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "backend=trace,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
